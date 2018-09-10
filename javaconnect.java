1607f3ef0a7fdbb9b1ac786fdf33f5079b183728
Garima_sharma <41118471+garimashrma@users.noreply.github.com>
2018-09-10 12:45:06 +0530

    Library Management System
    Minor Project on Library Management System

---
+++ javaconnect.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author garima
+ */
+
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class javaconnect {
+        Connection conn;
+public static Connection ConnecrDb(){
+try{
+Class.forName("org.sqlite.JDBC");
+Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\sqlite\\sqlitestudio-3.1.1\\SQLiteStudio\\Library");
+return conn;
+}catch(Exception e){
+JOptionPane.showMessageDialog(null, e);
+return null;
+
+}
+}}
+
